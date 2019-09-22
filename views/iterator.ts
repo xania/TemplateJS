fd3acf8a47cf1bc5d7bd8872f0a299755e12bae2
xania <ibrahim.bensalah@gmail.com>
2019-09-22 09:02:42 +0200

    check for null

--- views/iterator.ts
+++ views/iterator.ts
@@ -65,7 +65,7 @@ class IteratorTemplate<T> implements ITemplate {
                     }
                 }
 
-                return value.slice(0);
+                return value ? value.slice(0) : [];
             });
 
             return {
