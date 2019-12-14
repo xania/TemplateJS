e0db30c3154a18d5558bdab46cebc95c585f33ec
xania <ibrahim.bensalah@gmail.com>
2019-12-14 08:28:30 +0100

    refactor fragment

---
+++ views/list.ts
@@ -0,0 +1,126 @@
+import { ITemplate, Binding, IDriver } from "./driver";
+import arrayComparer from "storejs/src/array-comparer";
+import Store, { ProxyOf, flush, refresh } from "storejs"
+import { renderStack, asTemplate } from "templatejs/views";
+
+type Disposable = { dispose(): any };
+type ListSource<T> = {
+    value?: T[],
+    lift<U>(comparer: (newValue: T[], oldValue: U) => U): Disposable
+};
+type ItemTemplate<T> = (context: ProxyOf<T>, dispose: () => void) => ITemplate[];
+export default function List<T>(props: { source: ListSource<T> }, children: ItemTemplate<T>[]) {
+    return {
+        render(driver: IDriver) {
+            const { source } = props;
+
+            const scope = driver.createScope('--- Array ---');
+            const scopeDriver = scope.driver();
+
+            const childBindings: Binding[][] = [];
+            const childContexts: ProxyOf<T>[] = [];
+
+            const liftBinding = source.lift((newArray, prevArray: T[] = []) => {
+                const mutations = arrayComparer(newArray, prevArray);
+                for (var i = 0; i < mutations.length; i++) {
+                    const mut = mutations[i];
+                    if (mut.type === 'insert') {
+                        const { index } = mut;
+                        const childValue = newArray[index];
+                        const context = new Store<T>(childValue).asProxy();
+                        prevArray[index] = childValue;
+                        childContexts[index] = context;
+
+                        const args = [context, dispose];
+                        childBindings[index] = renderStack(
+                            flatTree(children, args).map(template => ({ driver: scopeDriver, template })).reverse()
+                        );
+
+                        function dispose() {
+                            const idx = childContexts.indexOf(context);
+                            // removeItem(newArray, childValue);
+                            // const idx = removeItem(prevArray, childValue);
+                            if (idx >= 0) {
+                                const bindings = childBindings[idx];
+
+                                childContexts.splice(idx, 1);
+                                childBindings.splice(idx, 1);
+                                prevArray.splice(idx, 1);
+                                newArray.splice(idx, 1);
+
+                                for (let b = 0; b < bindings.length; b++) {
+                                    bindings[b].dispose();
+                                }
+
+                                refresh(source);
+                            }
+                        }
+                    }
+                    else if (mut.type === 'remove') {
+                        const { index } = mut;
+                        const bindings = childBindings.splice(index, 1)[0];
+                        for (var e = 0; e < bindings.length; e++) {
+                            bindings[e].dispose();
+                        }
+                        prevArray.splice(index, 1);
+                        childContexts.splice(index, 1);
+                    }
+                    else if (mut.type === 'move') {
+                        const { from, to } = mut;
+                        swap(prevArray, from, to);
+                        swap(childContexts, from, to);
+                        swap(childBindings, from, to);
+                    }
+                }
+                return prevArray;
+            });
+
+            return {
+                dispose() {
+                    liftBinding.dispose();
+                    for (let i = 0; i < childBindings.length; i++) {
+                        const bindings = childBindings[i];
+                        for (let e = 0; e < bindings.length; e++) {
+                            bindings[e].dispose();
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    function flatTree<T>(source: T[], args: any[]): ITemplate[] {
+        const stack = [source as any];
+        const result: ITemplate[] = [];
+
+        while (stack.length > 0) {
+            const curr = stack.pop();
+            if (window.Array.isArray(curr)) {
+                for (let i = curr.length - 1; i >= 0; i--) {
+                    stack.push(curr[i]);
+                }
+            } else if (typeof curr === 'function') {
+                const retval = curr.apply(curr, args);
+                stack.push(retval);
+            } else {
+                result.push(asTemplate(curr) as ITemplate);
+            }
+        }
+
+        return result;
+    }
+
+    function swap(array: any[], from: number, to: number) {
+        const tmp = array[from];
+        array[from] = array[to];
+        array[to] = tmp;
+    }
+
+    function removeItem<T>(array: T[], item: T) {
+        const idx = array.indexOf(item);
+        if (idx >= 0) {
+            array.splice(idx, 1);
+        }
+        return idx;
+    }
+}
